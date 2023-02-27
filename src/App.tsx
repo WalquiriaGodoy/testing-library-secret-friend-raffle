72b4b6f53cc9b20c4c25cc5a9ed3a2345cce45eb
Walquiria Godoy <walquiriagodoy@gmail.com>
2023-02-26 23:11:05 -0300

    feat(secret-friend-sorter): first commit from test studies

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import Form from './components/Form';
+import Configuracao from './paginas/Configuracao';
+import Sorteio from './paginas/Sorteio';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <RecoilRoot>
+        <Routes>
+          <Route path='/' element={<Configuracao />} />
+          <Route path='/sorteio' element={<Sorteio />} />
+        </Routes>
+      </RecoilRoot>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/paginas/Configuracao.tsx
@@ -0,0 +1,14 @@
+import Form from '../components/Form';
+import ListaParticipantes from '../components/ListaParticipantes';
+import Rodape from '../components/Rodape';
+
+const Configuracao = () => {
+  return (
+    <>
+      <Form />
+      <ListaParticipantes />
+      <Rodape />
+    </>
+  );
+};
+export default Configuracao;
